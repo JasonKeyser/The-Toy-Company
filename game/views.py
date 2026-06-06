d8662c82e84170eba58a17423640fa42004350f5
Jason Keyser <jasonk2@vt.edu>
2026-06-06 13:51:26 -0400

    loan model updated and loan notification added

--- game/views.py
+++ game/views.py
@@ -151,14 +151,6 @@ def game(request):
     equipment = Equipment.objects.all()
     notifications = []
 
-    #set loan parameters
-    last_turn = Turn.objects.filter(player=player).order_by("-turn_number").first()
-    if last_turn:
-        trailing_ebitda = last_turn.EBITDA
-    else:
-        trailing_ebitda = 0
-    max_leverage_ratio = 10
-
     # loan = Loan.objects.create(
     #     player = player,
     #     interest_rate = .10,
@@ -168,11 +160,21 @@ def game(request):
     # )
 
     # trailing_ebitda = compute_trailing_avg_ebitda(player, turns=3)
+
+    #set loan parameters
+    last_turn = Turn.objects.filter(player=player).order_by("-turn_number").first()
+    if last_turn:
+        trailing_ebitda = last_turn.EBITDA
+    else:
+        trailing_ebitda = 0
+    max_leverage_ratio = 10
+
     minimum_credit_available = player.difficulty.min_loan_amount
     min_years_of_financial_history = player.difficulty.min_years_of_financial_history
     loan_offer = {
         "interest_rate": 0.10,
         "loan_length": 10,
+        "max_leverage_ratio": max_leverage_ratio,
         "max_credit": max( trailing_ebitda * max_leverage_ratio, minimum_credit_available ),
         "eligible": player.turn_number > min_years_of_financial_history,
     }
@@ -294,11 +296,10 @@ def game(request):
                 f"⚙️ {player.equipment_name} now installed! "
                 f"Cost per unit on all toys is reduced by {savings_pct}%."
             )
-        if last_turn == min_years_of_financial_history:
+        if player.turn_number == min_years_of_financial_history + 1 and player.difficulty.financing_enabled:
             notifications.append(
                 f"💵 {player.company_name} now eligible for debt financing! "
             )
-
         toyformset = UnitsFormSet(factory_space=player.factory_space, prefix="toys")
         coverageformset = CoverageFormSet(prefix="coverage")
         inv_expansion_form = FactoryExpansionForm()

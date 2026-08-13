30e641677d61c9bd5010970146666a3c91a9f534
Jason Keyser <jasonk2@vt.edu>
2026-08-12 20:15:22 -0400

    vendored driver.js files

--- users/models.py
+++ users/models.py
@@ -5,6 +5,7 @@ from PIL import Image
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_picture = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    has_seen_tutorial = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.user.username} Profile'
